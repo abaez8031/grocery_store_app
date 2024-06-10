fc7a1e2f8b7c5d7af3b8705282b76530289cc076
abaez8031 <abaez8031@gmail.com>
2024-06-10 04:15:15 -0400

    nextsteps

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { addToCart, removeFromCart, removeOneFromCart } from '../../store/cart';
+
+// When the number in the input field next to a cart item is changed and clicked off of, it should update the count for that cart item in the Redux store state.
+
+// If clicking the minus button or changing the number in the input field ever updates to a count lower than 1, remove the cart item.
+
 function CartItem({ item }) {
   const [count, setCount] = useState(item.count);
   const dispatch = useDispatch();
