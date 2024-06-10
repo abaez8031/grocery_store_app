6be0149c6af8022f464213d68ca6dbae0c2df0b0
abaez8031 <abaez8031@gmail.com>
2024-06-10 00:34:46 -0400

    removeproductsfromcart

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,16 +1,25 @@
 const ADD_TO_CART = "cart/ADD_TO_CART"
+const REMOVE_FROM_CART = "cart/REMOVE_FROM_CART"
 
 export const addToCart = id => ({
   type: ADD_TO_CART,
   id
 })
 
+export const removeFromCart = id => ({
+  type: REMOVE_FROM_CART,
+  id
+})
+
 const cartReducer = (state= {}, action) => {
+  const newState = {...state};
   switch(action.type) {
     case ADD_TO_CART:
-      const newState = {...state};
       newState[action.id] = {id: action.id, count: newState[action.id] ? newState[action.id].count + 1 : 1}
       return newState;
+    case REMOVE_FROM_CART:
+      delete newState[action.id]
+      return newState
     default:
       return state;
   }
